ec45df78971890fa89b799eca4b8dd2e2bf1bc8c
Isobel Pang <173300585+isobel-p@users.noreply.github.com>
2025-01-31 14:34:39 +0000

    Add lives and boundary collision]

--- Collision game.py
+++ Collision game.py
@@ -11,6 +11,7 @@ class Player:
         # center the player in the middle of the screen
         self.rect = pygame.Rect(self.x-50, self.y-50, 100, 100)
         self.speed = 4
+        self.lives = 3
     def move_x(self, value):
         self.x += value
     def move_y(self, value):
@@ -24,15 +25,6 @@ class Player:
         GOLD = (248, 200, 40)
         
         if skin == "safe" or skin == 0:
-##            pygame.draw.circle(screen, BLUE, [x,y], 20, 0) # draw a circle - centre x, centre y, radius, line wid
-##            pygame.draw.rect(screen, GOLD, pygame.Rect(x-20,y-20, 40, 10)) # draw a rectangle - screen, colour, ( top left x coord, top left y coord, width, height )
-##            pygame.draw.polygon(screen, RED, points=[(x-15,y-21), (x,y-40), (x+15,y-21)]) # draw a polygon - 1st coord, 2nd coord, 3rd coord
-##            #pygame.draw.line(screen, RED, (x,y), (x+120, y+60), 20) # draw a line - screen, colour, 1st coord, 2nd coord, width in pixels
-##            #pygame.draw.ellipse(screen, RED, (x, y, 200, 100), 0) #draw an ellipse - screen, colour, ( top left x coord, top left y coord , width, height), fill
-##            pygame.draw.polygon(screen, RED, points=[(x+10,y-40), (x+20,y-52), (x+56,y-22), (x+46,y-10)])
-##            self.rect = pygame.Rect(x-50, y-50, 100, 100)
-
-            
             rects.append(pygame.draw.rect(screen, GOLD, pygame.Rect(x-20, y-15, 40, 20)))
             rects.append(pygame.draw.rect(screen, BLUE, pygame.Rect(x-30, y-30, 20, 35)))
             rects.append(pygame.draw.rect(screen, RED, pygame.Rect(x-35, y-35, 30, 5)))
@@ -41,11 +33,7 @@ class Player:
             rects.append(pygame.draw.circle(screen, RED, [x+5, y+10], 5))
             rects.append(pygame.draw.circle(screen, RED, [x+20, y+10], 5))
 
-
-            
             self.rect = self.rect.unionall(rects)
-
-            print(rects)
         elif skin == "crashed" or skin == 1:
             pygame.draw.circle(screen, (255, 0, 0), [x,y], 30, 5) # draw a circle - centre x, centre y, radius, line width
 
@@ -110,14 +98,7 @@ while running:
             running = False
 
     keys = pygame.key.get_pressed() # listen for keypress
-##    if keys[pygame.K_UP]:
-##        player.move_y(-player.speed)
-##    if keys[pygame.K_DOWN]:
-##        player.move_y(player.speed)
-##    if keys[pygame.K_LEFT]:
-##        player.move_x(-player.speed)
-##    if keys[pygame.K_RIGHT]:
-##        player.move_x(player.speed)
+
     if keys[pygame.K_SPACE]:
         player.move_x(player.speed)
     else:
@@ -127,8 +108,16 @@ while running:
     if count % 30 == 0:
         points.append(Point())
         enemies.append(Enemy())
-
-    
+    if player.x < 0:
+        #print("Out of bounds")
+        player.lives -= 1
+        player.move_x(screen_size/2)
+        print(player.lives)
+    if player.x > screen_size:
+        #print("Out of bounds")
+        player.lives -= 1
+        player.move_x(-screen_size/2)
+        print(player.lives)
     # draw to the screen
     safeColour = (47, 181, 65) # add the safe background colour 
     screen.fill(safeColour)
@@ -137,14 +126,22 @@ while running:
         enemy.draw()
         enemy.move_y(5)
         if pygame.Rect.colliderect(player.rect, enemy.rect):
-            print("Hit!")
+            #print("Hit!")
             enemies.remove(enemy)
+            player.lives -= 1
+            print(player.lives)
     for point in points:
         point.draw()
         point.move_y(-5)
         if pygame.Rect.colliderect(player.rect, point.rect):
-            print("Point!")
+            #print("Point!")
             points.remove(point)
+            player.lives += 1
+            print(player.lives)
+    
+    
+
+    
     
     # Flip the display
     pygame.display.flip()
